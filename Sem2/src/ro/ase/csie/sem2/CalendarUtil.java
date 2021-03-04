3d07226d39e4b121d04652ca2c83aa87f0823b04
StirbescuSilviu <73109933+StirbescuSilviu@users.noreply.github.com>
2021-03-04 14:20:03 +0200

    1st commit

---
+++ Sem2/src/ro/ase/csie/sem2/CalendarUtil.java
@@ -0,0 +1,48 @@
+package ro.ase.csie.sem2;
+public class CalendarUtil {
+	
+	public String weekDay(int day) {
+		if(day==1)
+			return "Sunday";
+		else if(day==2)
+			return "Monday";
+		else if(day==3)
+			return "Tuesday";
+		else if(day==4)
+			return "Wednesday";
+		else if(day==5)
+			return "Thursday";
+		else if(day==6)
+			return "Friday";
+		else if(day==7)
+			return "Saturday";
+		return null;
+	}
+	public String weekDay2(int day) {
+		switch(day) {
+			case 1:
+			 return "Sunday";
+			case 2:
+				return "Monday";
+			case 3:
+				return "Tuesday";
+			case 4:
+				return "Wednesday";
+			case 5:
+				return "Thursday";
+			case 6:
+				return "Friday";
+			case 7:
+				return "Saturday";
+			default:
+				return null;
+		}
+	}
+	
+	public String weekDay3(int day) {
+		String[] week= {"Sunday", "Monday","Tuesday","Wednesday","Friday","Saturday"};
+		if(day>=0 || day <= 6)return week[day];
+		else return null;
+	}
+}
+
---
+++ Sem2/src/ro/ase/csie/sem2/Main.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.sem2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CalendarUtil cu=new CalendarUtil();
+		
+		System.out.println(cu.weekDay(3));
+		System.out.println(cu.weekDay2(3));
+		System.out.println(cu.weekDay2(4));
+	}
+
+}
