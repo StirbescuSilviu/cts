8a9f354ab57580af6e25b06b264dcdd9ea89d394
StirbescuSilviu <73109933+StirbescuSilviu@users.noreply.github.com>
2021-04-14 15:54:06 +0300

    Laborator8 adapter+decorator

--- DonaldDuck.java
+++ DonaldDuck.java
@@ -1,5 +1,45 @@
 package ro.ase.csie.cts.g1087.dp.adapter;
 
-public class DonaldDuck {
+public class DonaldDuck implements ActiuniDisney {
+
+		public static final String denumire="DONALD DUCK";
+		int power;
+	@Override
+	public void move(int x, int y) {
+	System.out.println("repozitionare caracter la noile coordonate");
+		
+	}
+
+	@Override
+	public void primesteLovitura(int power) {
+		this.power-=power;
+		System.out.println("nivel putere scade la "+ this.power);
+		
+	}
+
+	@Override
+	public void reincarca(int power) {
+		this.power+=power;
+		System.out.println("nivel putere reincarcat la "+ this.power);
+		
+	}
+
+	@Override
+	public void jump() {
+		System.out.println("Jump");
+		
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public static String getDenumire() {
+		return denumire;
+	}
 
 }
