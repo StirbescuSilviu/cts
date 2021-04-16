8f98c92239309b1cd75eea95adcea98e1998b0c6
StirbescuSilviu <73109933+StirbescuSilviu@users.noreply.github.com>
2021-04-16 11:25:27 +0300

    Flyweight
    Flyweight

---
+++ Laborator5-Recuperare Composit Flyweight/src/flyweight/InterfataModel3D.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.g1087.dp.flyweight;
+
+public interface InterfataModel3D {
+
+	public void incarcaModel3d(String fisier );
+	public void afisareEcran(ContextEcran context);
+}
