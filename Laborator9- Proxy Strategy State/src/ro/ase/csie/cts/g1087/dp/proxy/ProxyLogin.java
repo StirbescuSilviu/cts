ec96d4263a49eee072fdcba61144a78fc96d92ba
StirbescuSilviu <73109933+StirbescuSilviu@users.noreply.github.com>
2021-04-21 14:45:52 +0300

    Proxy
    Proxy

---
+++ Laborator9- Proxy Strategy State/src/ro/ase/csie/cts/g1087/dp/proxy/ProxyLogin.java
@@ -0,0 +1,53 @@
+package ro.ase.csie.cts.g1087.dp.proxy;
+
+import java.nio.channels.UnsupportedAddressTypeException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProxyLogin implements InterfataModulLogin {
+
+	InterfataModulLogin modulLogin= null;
+	
+	public static final int NR_MAX_INCERCARI = 3;
+	public Map<String,Integer> nrIncercari = new HashMap<>();
+	
+	@Override
+	public boolean login(String username, String pass) {
+		int nrIncercariNereusite = nrIncercari.get(username);
+		if (nrIncercariNereusite == 0) {
+			nrIncercari.put(username, 0);
+			nrIncercariNereusite = 0;
+		}
+
+		if (modulLogin != null) {
+			if (nrIncercariNereusite < NR_MAX_INCERCARI) {
+				if (modulLogin.login(username, pass) == true) {
+					nrIncercariNereusite=0;
+					return true;
+					}
+				else {
+					nrIncercariNereusite += 1;
+					nrIncercari.put(username,nrIncercariNereusite);
+					return false;
+				}
+			} else
+				return false;
+		} else
+			throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean verificaStatusServer() {
+		if(modulLogin!= null)
+			return modulLogin.verificaStatusServer();
+		else
+			throw new UnsupportedOperationException();
+	}
+
+	public ProxyLogin(InterfataModulLogin modulLogin) {
+		super();
+		this.modulLogin = modulLogin;
+	}
+
+	
+}
