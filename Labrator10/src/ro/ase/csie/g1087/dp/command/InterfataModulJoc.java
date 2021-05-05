264ee5a9ca938327858647b3762cf910a2c21985
StirbescuSilviu <73109933+StirbescuSilviu@users.noreply.github.com>
2021-05-05 15:36:10 +0300

    Implementare command fara manager
    Implementare command fara manager

---
+++ Labrator10/src/ro/ase/csie/g1087/dp/command/InterfataModulJoc.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.g1087.dp.command;
+
+public interface InterfataModulJoc {
+
+}
---
+++ Labrator10/src/ro/ase/csie/g1087/dp/command/TestCommand.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.g1087.dp.command;
+
+public class TestCommand {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
