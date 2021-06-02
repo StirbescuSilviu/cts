ae0cd667910be2ed68a71cb77b0332f8c8e72762
StirbescuSilviu <73109933+StirbescuSilviu@users.noreply.github.com>
2021-06-02 15:48:00 +0300

    Test JUnit partea 4
    Test JUnit partea 4

---
+++ Laboratorul 14/src/ro/ase/cts/g1087/testare/exceptii/ExceptieNota.java
@@ -0,0 +1,4 @@
+package ro.ase.cts.g1087.testare.exceptii;
+
+public class ExceptieNota extends Exception {
+}
---
+++ Laboratorul 14/src/ro/ase/cts/g1087/testare/exceptii/ExceptieNume.java
@@ -0,0 +1,4 @@
+package ro.ase.cts.g1087.testare.exceptii;
+
+public class ExceptieNume extends Exception {
+}
